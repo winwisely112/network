7a704c9990eb42fb020a886c931ef0ec31ce935b
ged <winwisely99@gmail.com>
2019-11-05 15:53:45 +0100

    forms

--- cloud/forms/type/user.go
+++ cloud/forms/type/user.go
@@ -5,6 +5,7 @@ import (
 	// "github.com/go-playground/form"
 	"github.com/go-playground/form"
 )
+
 // UserDTO get user details for signup
 type UserDTO struct {
 	UserName  string `form:"userName,omitempty"`
